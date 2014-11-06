518e007db00096d91eca1f149c395d01abf5475c
Jeff Hoogland <jeffhoogland@linux.com>
2014-11-06 13:09:51 -0600

    Data formatting and stats by opponent implemented

--- qutemtgstats.py
+++ qutemtgstats.py
@@ -1,6 +1,24 @@
+#How many lines after the data we find certain data
+EventDate = 0
+EventType = 1
+EventPlayers = 3
+EventFormat = 5
+EventLocation = 6
+EventPlace = 7
+EventID = 8
+EventHistory = 10
+
 import sys
 import os
 import platform
+import datetime
+
+def checkDate(date_text):
+    try:
+        datetime.datetime.strptime(date_text, '%Y-%m-%d')
+        return True
+    except ValueError:
+        return False
 
 from PySide.QtGui import *
 from PySide.QtCore import *
@@ -11,6 +29,105 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
+        self.eventData = {}
+        self.opponentData = {}
+        self.assignWidgets()
+    
+    def phrasePressed( self ):
+        self.rawData = self.statsPaste.toPlainText().split("\n")
+        
+        self.formatData()
+        
+    def formatData( self ):
+        eventBreaks = []
+        numberOfRows = len(self.rawData)
+        
+        for row in range(numberOfRows):
+            if checkDate(self.rawData[row-1][:10]):
+                eventBreaks.append(row-1)
+        
+        for event in eventBreaks:
+            startingRow = event
+            
+            if eventBreaks.index(event)+1 < len(eventBreaks):
+                endingRow = eventBreaks[eventBreaks.index(event) + 1]
+            else:
+                endingRow = len(self.rawData)
+            
+            eventId = self.rawData[startingRow+EventID].split(":")[1].lstrip()
+            eventPlace = self.rawData[startingRow+EventPlace].split(":")[1].lstrip()
+            eventType = self.rawData[startingRow+EventType].split(":")[1].lstrip()
+            eventPlayers = self.rawData[startingRow+EventPlayers].split(":")[1].lstrip()
+            eventFormat = self.rawData[startingRow+EventFormat].split(":")[1].lstrip()
+            eventLocation = self.rawData[startingRow+EventLocation].split(":")[1].lstrip()
+            eventDate = self.rawData[startingRow+EventDate][:10]
+            eventWins = 0
+            eventLosses = 0
+            eventDraws = 0
+            
+            for row in range(startingRow+EventHistory, endingRow):
+                ourRound = self.rawData[row].split("\t")
+                if len(ourRound) == 4:
+                    if len(ourRound[3]):
+                        self.addMatch(ourRound[3], ourRound[1])
+                        if ourRound[1] == "Win":
+                            eventWins += 1
+                        elif ourRound[1] == "Loss":
+                            eventLosses += 1
+                        elif ourRound[1] == "Draw":
+                            eventDraws += 1
+                        
+            self.eventData[eventId] = { "Place":eventPlace,
+                                        "Type":eventType,
+                                        "Players":eventPlayers,
+                                        "Format":eventFormat,
+                                        "Location":eventLocation,
+                                        "Date":eventDate,
+                                        "Wins":eventWins,
+                                        "Losses":eventLosses,
+                                        "Draws":eventDraws}
+                                        
+        self.updateGUI()
+        
+    def updateGUI( self ):
+        #Update opponents
+        __sortingEnabled = self.treeWidget.isSortingEnabled()
+        self.treeWidget.setSortingEnabled(False)
+        for opponent in self.opponentData:
+            opponentItem =  TreeWidgetItem(self.treeWidget)
+            opponentItem.setText(0, opponent)
+            opponentItem.setText(1, unicode(self.opponentData[opponent]["Win"]))
+            opponentItem.setText(2, unicode(self.opponentData[opponent]["Loss"]))
+            opponentItem.setText(3, unicode(self.opponentData[opponent]["Draw"]))
+            opponentItem.setText(4, unicode(self.opponentData[opponent]["Win"]+self.opponentData[opponent]["Loss"]+self.opponentData[opponent]["Draw"]))
+            
+            self.treeWidget.addTopLevelItem(opponentItem)
+        
+        self.treeWidget.setSortingEnabled(__sortingEnabled)
+                        
+    def addMatch( self, opponent, result ):
+        """opponent is the opponent's name and result should be Win/Loss/Draw"""
+        
+        if opponent not in self.opponentData:
+            self.opponentData[opponent] = {  "Win":0,
+                                            "Loss":0,
+                                            "Draw":0}
+        
+        self.opponentData[opponent][result] += 1
+    
+    def assignWidgets( self ):
+        self.phraseButton.clicked.connect(self.phrasePressed)
+
+class TreeWidgetItem( QTreeWidgetItem ):
+    def __init__(self, parent=None):
+        QTreeWidgetItem.__init__(self, parent)
+
+    def __lt__(self, otherItem):
+        column = self.treeWidget().sortColumn()
+        try:
+            return float( self.text(column) ) > float( otherItem.text(column) )
+        except ValueError:
+            return self.text(column) > otherItem.text(column)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- ui_Main.py
+++ ui_Main.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'Main.ui'
 #
-# Created: Thu Nov  6 00:46:08 2014
+# Created: Thu Nov  6 12:56:59 2014
 #      by: pyside-uic 0.2.15 running on PySide 1.2.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,25 +12,60 @@ from PySide import QtCore, QtGui
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(800, 535)
+        MainWindow.setMinimumSize(QtCore.QSize(800, 535))
+        MainWindow.setMaximumSize(QtCore.QSize(800, 535))
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.textEdit = QtGui.QTextEdit(self.centralwidget)
-        self.textEdit.setGeometry(QtCore.QRect(0, 10, 801, 531))
-        self.textEdit.setAcceptRichText(False)
-        self.textEdit.setObjectName("textEdit")
+        self.tabWidget = QtGui.QTabWidget(self.centralwidget)
+        self.tabWidget.setGeometry(QtCore.QRect(0, 0, 801, 535))
+        self.tabWidget.setObjectName("tabWidget")
+        self.dataTab = QtGui.QWidget()
+        self.dataTab.setObjectName("dataTab")
+        self.statsPaste = QtGui.QTextEdit(self.dataTab)
+        self.statsPaste.setGeometry(QtCore.QRect(0, 40, 795, 401))
+        self.statsPaste.setAcceptRichText(False)
+        self.statsPaste.setObjectName("statsPaste")
+        self.phraseButton = QtGui.QToolButton(self.dataTab)
+        self.phraseButton.setGeometry(QtCore.QRect(10, 450, 781, 41))
+        self.phraseButton.setObjectName("phraseButton")
+        self.label = QtGui.QLabel(self.dataTab)
+        self.label.setGeometry(QtCore.QRect(250, 0, 261, 41))
+        self.label.setObjectName("label")
+        self.tabWidget.addTab(self.dataTab, "")
+        self.formatTab = QtGui.QWidget()
+        self.formatTab.setObjectName("formatTab")
+        self.tabWidget.addTab(self.formatTab, "")
+        self.eventTab = QtGui.QWidget()
+        self.eventTab.setObjectName("eventTab")
+        self.tabWidget.addTab(self.eventTab, "")
+        self.opponentTab = QtGui.QWidget()
+        self.opponentTab.setObjectName("opponentTab")
+        self.treeWidget = QtGui.QTreeWidget(self.opponentTab)
+        self.treeWidget.setGeometry(QtCore.QRect(0, 0, 795, 501))
+        self.treeWidget.setObjectName("treeWidget")
+        self.tabWidget.addTab(self.opponentTab, "")
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 29))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtGui.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        self.actionMenu_Item = QtGui.QAction(MainWindow)
+        self.actionMenu_Item.setObjectName("actionMenu_Item")
 
         self.retranslateUi(MainWindow)
+        self.tabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
+        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "Qute MTG Stats", None, QtGui.QApplication.UnicodeUTF8))
+        self.phraseButton.setText(QtGui.QApplication.translate("MainWindow", "Phrase Data and Generate Stats", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:18pt; font-weight:600;\">Paste Data Below:</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.dataTab), QtGui.QApplication.translate("MainWindow", "Raw Data", None, QtGui.QApplication.UnicodeUTF8))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.formatTab), QtGui.QApplication.translate("MainWindow", "Stats by Format", None, QtGui.QApplication.UnicodeUTF8))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.eventTab), QtGui.QApplication.translate("MainWindow", "Stats by Event Type", None, QtGui.QApplication.UnicodeUTF8))
+        self.treeWidget.setSortingEnabled(True)
+        self.treeWidget.headerItem().setText(0, QtGui.QApplication.translate("MainWindow", "Opponent", None, QtGui.QApplication.UnicodeUTF8))
+        self.treeWidget.headerItem().setText(1, QtGui.QApplication.translate("MainWindow", "Wins", None, QtGui.QApplication.UnicodeUTF8))
+        self.treeWidget.headerItem().setText(2, QtGui.QApplication.translate("MainWindow", "Losses", None, QtGui.QApplication.UnicodeUTF8))
+        self.treeWidget.headerItem().setText(3, QtGui.QApplication.translate("MainWindow", "Draws", None, QtGui.QApplication.UnicodeUTF8))
+        self.treeWidget.headerItem().setText(4, QtGui.QApplication.translate("MainWindow", "Total", None, QtGui.QApplication.UnicodeUTF8))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.opponentTab), QtGui.QApplication.translate("MainWindow", "Stats by Opponent", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionMenu_Item.setText(QtGui.QApplication.translate("MainWindow", "Menu Item", None, QtGui.QApplication.UnicodeUTF8))
 
