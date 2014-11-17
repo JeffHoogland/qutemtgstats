bf9492c07c2d4d93f86da4fab2195596c612cb99
Jeff <jeffhoogland@linux.com>
2014-11-17 10:45:43 -0600

    Added deck stats pages

---
+++ Code/DeckStatsWindow.py
@@ -0,0 +1,51 @@
+import os
+
+from PySide.QtGui import *
+from PySide.QtCore import *
+
+from ui_DeckStats import Ui_DeckStats
+
+class DeckStatsWindow(QDialog, Ui_DeckStats):
+    def __init__(self, parent):
+        super(DeckStatsWindow, self).__init__(parent)
+        self.setupUi(self)
+        self.frames = {}
+        self.assignWidgets()
+        self.rent = parent
+
+    def updateGUI( self ):
+        for frame in self.frames:
+            self.frames[frame][0].hide()
+
+        self.frames.clear()
+
+        for deck in self.rent.selectedDecks:
+            #Each entry is a list with the first being a QFrame object, the second being a QVBoxLayout object, and 3-6 being QLabels
+            self.frames[deck] = []
+            self.frames[deck].append(QFrame(self.scrollAreaWidgetContents_2))
+            self.frames[deck][0].setFrameShape(QFrame.StyledPanel)
+            self.frames[deck][0].setFrameShadow(QFrame.Raised)
+            self.frames[deck][0].setObjectName("%sFrame"%deck)
+
+            self.frames[deck].append(QVBoxLayout(self.frames[deck][0]))
+
+            self.frames[deck].append(QLabel(self.frames[deck][0]))
+            self.frames[deck][2].setText("<html><head/><body><p><span style=\" font-size:16pt; font-weight:600;\">%s:</span></p></body></html>"%deck)
+            self.frames[deck].append(QLabel(self.frames[deck][0]))
+            self.frames[deck][3].setText("<p><b>Overall Record:</b></p> <p>%s-%s-%s</p>"%(self.rent.statsDecks[deck]["Wins"], self.rent.statsDecks[deck]["Losses"], self.rent.statsDecks[deck]["Draws"]))
+            self.frames[deck].append(QLabel(self.frames[deck][0]))
+            self.frames[deck][4].setText("<p><b>Total Matches:</b></p> <p>%s</p>"%self.rent.statsDecks[deck]["Matches"])
+            self.frames[deck].append(QLabel(self.frames[deck][0]))
+            self.frames[deck][5].setText("<p><b>Win Percent:</b></p> <p>%.2f</p>"%self.rent.statsDecks[deck]["Win Percent"])
+
+            for i in range(2, 6):
+                self.frames[deck][1].addWidget(self.frames[deck][i])
+
+            self.horizontalLayout_6.addWidget(self.frames[deck][0])
+
+    def cancelPressed( self ):
+        self.hide()
+
+    def assignWidgets( self ):
+        self.cancelButton.clicked.connect(self.cancelPressed)
+        self.exportStatsButton.clicked.connect(lambda: self.rent.exportStatsPressed())
---
+++ Code/ui_DeckStats.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'UIs/DeckStats.ui'
+#
+# Created: Sun Nov 16 22:11:42 2014
+#      by: pyside-uic 0.2.15 running on PySide 1.2.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore, QtGui
+
+class Ui_DeckStats(object):
+    def setupUi(self, DeckStats):
+        DeckStats.setObjectName("DeckStats")
+        DeckStats.resize(400, 300)
+        self.verticalLayout = QtGui.QVBoxLayout(DeckStats)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.scrollArea_2 = QtGui.QScrollArea(DeckStats)
+        self.scrollArea_2.setWidgetResizable(True)
+        self.scrollArea_2.setObjectName("scrollArea_2")
+        self.scrollAreaWidgetContents_2 = QtGui.QWidget()
+        self.scrollAreaWidgetContents_2.setGeometry(QtCore.QRect(0, 0, 378, 223))
+        self.scrollAreaWidgetContents_2.setObjectName("scrollAreaWidgetContents_2")
+        self.horizontalLayout_6 = QtGui.QHBoxLayout(self.scrollAreaWidgetContents_2)
+        self.horizontalLayout_6.setObjectName("horizontalLayout_6")
+        self.scrollArea_2.setWidget(self.scrollAreaWidgetContents_2)
+        self.verticalLayout.addWidget(self.scrollArea_2)
+        self.frame = QtGui.QFrame(DeckStats)
+        self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtGui.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.horizontalLayout = QtGui.QHBoxLayout(self.frame)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.exportStatsButton = QtGui.QPushButton(self.frame)
+        self.exportStatsButton.setObjectName("exportStatsButton")
+        self.horizontalLayout.addWidget(self.exportStatsButton)
+        self.cancelButton = QtGui.QPushButton(self.frame)
+        self.cancelButton.setObjectName("cancelButton")
+        self.horizontalLayout.addWidget(self.cancelButton)
+        self.verticalLayout.addWidget(self.frame)
+
+        self.retranslateUi(DeckStats)
+        QtCore.QMetaObject.connectSlotsByName(DeckStats)
+
+    def retranslateUi(self, DeckStats):
+        DeckStats.setWindowTitle(QtGui.QApplication.translate("DeckStats", "Qute Deck Stats", None, QtGui.QApplication.UnicodeUTF8))
+        self.exportStatsButton.setText(QtGui.QApplication.translate("DeckStats", "Export Stats", None, QtGui.QApplication.UnicodeUTF8))
+        self.cancelButton.setText(QtGui.QApplication.translate("DeckStats", "Done", None, QtGui.QApplication.UnicodeUTF8))
+
